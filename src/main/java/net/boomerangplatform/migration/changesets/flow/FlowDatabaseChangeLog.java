c8386732665e5dbc2c090177335472b82b6ce7c7
amhudson <amhudson@us.ibm.com>
2021-07-29 16:36:25 -0500

    update updateTaskTemplateRevisionCommand step

--- src/main/java/net/boomerangplatform/migration/changesets/flow/FlowDatabaseChangeLog.java
+++ src/main/java/net/boomerangplatform/migration/changesets/flow/FlowDatabaseChangeLog.java
@@ -1107,18 +1107,22 @@ public class FlowDatabaseChangeLog {
 
       List<Document> revisions = (List<Document>) taskTemplate.get("revisions");
       for (Document revision : revisions) {
-        List<String> updatedCommand = new ArrayList<>();
-        String command = revision.getString("command");
-        if (command != null && !command.isBlank()) {
-          updatedCommand.add(command);
+        try {
+          String command = revision.getString("command");
+          List<String> updatedCommand = new ArrayList<>();
+          if (command != null && !command.isBlank()) {
+            updatedCommand.add(command);
+          }
+          revision.put("command", updatedCommand);
+          collection.replaceOne(eq("_id", taskTemplate.getObjectId("_id")), taskTemplate);
+        } catch (ClassCastException e) {
+
         }
-        revision.put("command", updatedCommand);
-        collection.replaceOne(eq("_id", taskTemplate.getObjectId("_id")), taskTemplate);
       }
     }
 
   }
-  
+
   @ChangeSet(order = "066", id = "066", author = "Adrienne Hudson")
   public void tasktemplateUpdate(MongoDatabase db) throws IOException {
 
