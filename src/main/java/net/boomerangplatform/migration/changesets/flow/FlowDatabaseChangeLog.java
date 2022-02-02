ad5d6b06725fa81f5211b0117ef64a9db80451d1
amhudson <amhudson@us.ibm.com>
2022-02-02 10:29:28 -0600

    update default worker image

--- src/main/java/net/boomerangplatform/migration/changesets/flow/FlowDatabaseChangeLog.java
+++ src/main/java/net/boomerangplatform/migration/changesets/flow/FlowDatabaseChangeLog.java
@@ -1621,4 +1621,20 @@ public class FlowDatabaseChangeLog {
       collection.insertOne(doc);
     }
   }
+  
+  @ChangeSet(order = "088", id = "088", author = "Adrienne Hudson")
+  public void updatedefaultworker(MongoDatabase db) throws IOException {
+    MongoCollection<Document> collection = db.getCollection(collectionPrefix + "settings");
+    Document workers = collection.find(eq("name", "Task Configuration")).first();
+    List<Document> configs = (List<Document>) workers.get("config");
+
+    for (Document config : configs) {
+      if (config.get("key").equals("worker.image")) {
+        config.put("value", "boomerangio/worker-flow:2.10.16");
+      }
+    }
+
+    workers.put("config", configs);
+    collection.replaceOne(eq("name", "Task Configuration"), workers);
+  }
 }
