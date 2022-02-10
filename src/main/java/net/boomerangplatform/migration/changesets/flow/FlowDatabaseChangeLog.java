995e0545f235eb4fe267177f7fa134d5645f8e7f
amhudson <amhudson@us.ibm.com>
2022-02-10 07:51:35 -0600

    update task template

--- src/main/java/net/boomerangplatform/migration/changesets/flow/FlowDatabaseChangeLog.java
+++ src/main/java/net/boomerangplatform/migration/changesets/flow/FlowDatabaseChangeLog.java
@@ -1669,4 +1669,17 @@ public class FlowDatabaseChangeLog {
       collection.insertOne(doc);
     }
   }
+  
+  @ChangeSet(order = "093", id = "093", author = "Adrienne Hudson")
+  public void updatingtemplate(MongoDatabase db) throws IOException {
+
+    MongoCollection<Document> collection = db.getCollection(collectionPrefix + "task_templates");
+
+    final List<String> files = fileloadingService.loadFiles("flow/093/flow_task_templates/*.json");
+    for (final String fileContents : files) {
+      final Document doc = Document.parse(fileContents);
+      collection.findOneAndDelete(eq("_id", doc.getObjectId("_id")));
+      collection.insertOne(doc);
+    }
+  }
 }
